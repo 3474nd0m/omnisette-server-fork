c5cb554008a49992023a491b4929dbef36721352
3474nd0m <marcodouangvilayiii@gmail.com>
2026-09-24 03:18:47 +1000

    debug: add startup echo to diagnose Render exit 128

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+echo "DEBUG: Entrypoint running as user $(id -u), attempting to bind to port ${PORT:-10000}"
 
 exec /opt/omnisette-server/omnisette-server --http-port "${PORT:-10000}" --https-port 8443 -l Info
