961f5942dbff96b7538dba878ac268ec36989745
3474nd0m <marcodouangvilayiii@gmail.com>
2026-09-24 03:20:44 +1000

    simplify entrypoint to absolute binary path only

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-echo "DEBUG: Entrypoint running as user $(id -u), attempting to bind to port ${PORT:-10000}"
+
+echo "DEBUG: uid=$(id -u) cwd=$(pwd) port=${PORT:-10000}"
 
 exec /opt/omnisette-server/omnisette-server --http-port "${PORT:-10000}" --https-port 8443 -l Info
