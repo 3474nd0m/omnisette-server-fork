ece8fa7475d6644d40e10d673beb6b7b06b9210b
3474nd0m <marcodouangvilayiii@gmail.com>
2026-09-24 03:13:36 +1000

    simplify entrypoint: remove redundant download

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,18 +1,3 @@
 #!/bin/bash
 
-if [ -z "$(ls lib/ 2> /dev/null)" ]; then
-  curl https://apps.mzstatic.com/content/android-apple-music-apk/applemusic.apk -O
-  if [ "$(uname -m)" = 'aarch64' ]; then
-    unzip applemusic.apk 'lib/arm64-v8a/libstoreservicescore.so' 'lib/arm64-v8a/libCoreADI.so'
-  elif [[ "$(uname -m)" = 'arm'* ]]; then
-    unzip applemusic.apk 'lib/armeabi-v7a/libstoreservicescore.so' 'lib/armeabi-v7a/libCoreADI.so'
-  elif [ "$(uname -m)" = 'x86_64' ]; then
-    unzip applemusic.apk 'lib/x86_64/libstoreservicescore.so' 'lib/x86_64/libCoreADI.so'
-  elif [ "$(uname -m)" = 'i686' ]; then
-    unzip applemusic.apk 'lib/x86/libstoreservicescore.so' 'lib/x86/libCoreADI.so'
-  fi
-  rm applemusic.apk
-fi
-
-echo "DEBUG: Entrypoint running as user $(id -u), attempting to bind to port ${PORT:-10000}"
-exec ./omnisette-server --http-port "${PORT:-10000}" --https-port 8443 -l Info
+exec /opt/omnisette-server/omnisette-server --http-port "${PORT:-10000}" --https-port 8443 -l Info
